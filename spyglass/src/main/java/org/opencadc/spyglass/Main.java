7d12d699104fbb989afae291a33ffed1f8c2d330
Patrick Dowler <pdowler.cadc@gmail.com>
2023-10-24 18:33:30 -0700

    spyglass: initial commit

---
+++ spyglass/src/main/java/org/opencadc/spyglass/Main.java
@@ -0,0 +1,192 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2020.                            (c) 2020.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+************************************************************************
+*/
+
+package org.opencadc.spyglass;
+
+import ca.nrc.cadc.auth.AuthenticationUtil;
+import ca.nrc.cadc.auth.AuthorizationToken;
+import ca.nrc.cadc.auth.CertCmdArgUtil;
+import ca.nrc.cadc.util.ArgumentMap;
+import ca.nrc.cadc.util.Log4jInit;
+import ca.nrc.cadc.vos.Direction;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.AccessControlException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.security.auth.Subject;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author pdowler
+ */
+public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        try {
+            ArgumentMap am = new ArgumentMap(args);
+            Log4jInit.setLevel("org.opencadc", Level.WARN);
+            Log4jInit.setLevel("ca.nrc.cadc", Level.WARN);
+            if (am.isSet("v") || am.isSet("verbose")) {
+                Log4jInit.setLevel("org.opencadc.spyglass", Level.INFO);
+            } else if (am.isSet("d") || am.isSet("debug")) {
+                Log4jInit.setLevel("org.opencadc.spyglass", Level.DEBUG);
+                Log4jInit.setLevel("ca.nrc.cadc.reg", Level.DEBUG);
+                Log4jInit.setLevel("ca.nrc.cadc.vos", Level.DEBUG);
+                Log4jInit.setLevel("ca.nrc.cadc.net", Level.DEBUG);
+            } 
+                
+            boolean help = am.isSet("h") || am.isSet("help");
+            String r = am.getValue("resourceID");
+            List<String> pa = am.getPositionalArgs();
+            if (help || r == null || pa.size() != 2) {
+                usage();
+                System.exit(0);
+            }
+            String d = pa.get(0);
+            String t = pa.get(1);
+            String secStr = am.getValue("securityMethod");
+            String sidStr = am.getValue("standardID");
+            
+                        
+            final Direction direction;
+            if (Direction.pullFromVoSpace.getValue().equals(d)) {
+                direction = Direction.pullFromVoSpace;
+            } else if (Direction.pushToVoSpace.getValue().equals(d)) {
+                direction = Direction.pushToVoSpace;
+            } else {
+                throw new IllegalArgumentException("invalid direction: " + d);
+            }
+            
+            final URI resourceID = new URI(r);
+            final URI target = new URI(t);
+            URI sec = null;
+            if (secStr != null) {
+                sec = new URI(secStr);
+            }
+            URI sid = null;
+            if (sidStr != null) {
+                sid = new URI(sidStr);
+            }
+            
+            Subject subject = AuthenticationUtil.getAnonSubject();
+            if (am.isSet("cert")) {
+                subject = CertCmdArgUtil.initSubject(am);
+            } else if (am.isSet("token")) {
+                subject = getSubjectWithToken(am.getValue("token"), am.getValue("domain"));
+            }
+            
+            Spyglass spy = new Spyglass(resourceID, target, direction);
+            spy.securityMethod = sec;
+            spy.standardID = sid;
+            Subject.doAs(subject, spy);
+            
+        } catch (AccessControlException ex) {
+            log.error(ex.getMessage());
+            usage();
+            System.exit(-1);
+        } catch (IllegalArgumentException | URISyntaxException ex) {
+            log.error(ex);
+            usage();
+        } catch (Exception unexpected) {
+            log.error("FAIL", unexpected);
+            System.exit(-1);
+        }
+    }
+    
+    private static void usage() {
+        System.out.println("usage: spyglass [options] --resourceID=<resourceID> <direction> <target> ");
+        System.out.println("                direction : pullFromVoSpace | pushToVoSpace");
+        // TODO: add direction = biDirectional
+        System.out.println("                target : an Artifact.uri");
+        System.out.println("options:        [-v|--verbose|-d|--debug]");
+        System.out.println("                [--cert=<proxy cert>]");
+        System.out.println("                [--token<access token> --domain=<server or domain name>]");
+        System.out.println("                [--securityMethod=<uri>] : only request these security methods");
+        System.out.println("                [--protocol=<uri>] : request this protocol (e.g. standardID of an API)");
+        System.out.println("   example for raw SI locate: --protocol=http://www.opencadc.org/std/storage#files-1.0");
+        
+    }
+    
+    private static Subject getSubjectWithToken(String token, String domain) {
+        String[] ss = token.split(":");
+        String tokenType = ss[0];
+        String tokenValue = ss[1];
+        List<String> domains = new ArrayList<>();
+        domains.add(domain);
+        AuthorizationToken atk = new AuthorizationToken(tokenType, tokenValue, domains);
+        Subject s = new Subject();
+        s.getPublicCredentials().add(atk);
+        return s;
+    }
+    
+    private Main() {
+    }
+}
---
+++ spyglass/src/main/java/org/opencadc/spyglass/Spyglass.java
@@ -0,0 +1,241 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2020.                            (c) 2020.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+************************************************************************
+*/
+
+package org.opencadc.spyglass;
+
+import ca.nrc.cadc.auth.AuthMethod;
+import ca.nrc.cadc.auth.AuthenticationUtil;
+import ca.nrc.cadc.net.FileContent;
+import ca.nrc.cadc.net.HttpGet;
+import ca.nrc.cadc.net.HttpPost;
+import ca.nrc.cadc.net.ResourceNotFoundException;
+import ca.nrc.cadc.reg.Capabilities;
+import ca.nrc.cadc.reg.Capability;
+import ca.nrc.cadc.reg.Interface;
+import ca.nrc.cadc.reg.Standards;
+import ca.nrc.cadc.reg.client.RegistryClient;
+import ca.nrc.cadc.util.StringUtil;
+import ca.nrc.cadc.vos.Direction;
+import ca.nrc.cadc.vos.Protocol;
+import ca.nrc.cadc.vos.Transfer;
+import ca.nrc.cadc.vos.TransferReader;
+import ca.nrc.cadc.vos.TransferWriter;
+import ca.nrc.cadc.vos.VOS;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author pdowler
+ */
+public class Spyglass implements PrivilegedExceptionAction<Void> {
+    private static final Logger log = Logger.getLogger(Spyglass.class);
+
+    private final URI resourceID;
+    private final URI target;
+    private final Direction direction;
+    
+    // negotiation options
+    public URI securityMethod;
+    public URI standardID;
+
+    public Spyglass(URI resourceID, URI target, Direction direction) {
+        this.resourceID = resourceID;
+        this.target = target;
+        this.direction = direction;
+    }
+
+   
+
+    @Override
+    public Void run() throws Exception {
+        doit();
+        return null;
+    }
+    
+    public void doit() throws Exception {
+        final AuthMethod am = AuthenticationUtil.getAuthMethodFromCredentials(AuthenticationUtil.getCurrentSubject());
+        final URI sec = Standards.getSecurityMethod(am);
+        
+        long t1 = System.currentTimeMillis();
+        URL turl = getTN(sec);
+        log.info("transfer negotiation: " + turl);
+        
+        Transfer trans = negotiate(turl);
+        long dt = System.currentTimeMillis() - t1;
+        
+        log.info("found " + trans.getProtocols().size() + " " + trans.getDirection().getValue() + " URLs in " + dt + "ms");
+        for (Protocol p : trans.getProtocols()) {
+            System.out.println();
+            System.out.print(p.getEndpoint());
+            URI sm = p.getSecurityMethod();
+            if (sm == null) {
+                System.out.println(" [anon]");
+            } else {
+                System.out.println(" [" + sm.toASCIIString() + "]");
+            }
+        }
+    }
+    
+    private URL getTN(URI securityMethod) {
+        try {
+            // find transfer negotiation endpoint
+            RegistryClient reg = new RegistryClient();
+            log.info("capabilities: " + reg.getAccessURL(resourceID));
+            
+            Capabilities caps = reg.getCapabilities(resourceID);
+            Capability cap = caps.findCapability(Standards.VOSPACE_SYNC_21);
+            if (cap == null) {
+                cap = caps.findCapability(Standards.SI_LOCATE);
+            }
+            if (cap == null) {
+                throw new RuntimeException("resource " + resourceID + " does not implement a transfer negotiation API");
+            }
+
+            Interface iface = cap.findInterface(securityMethod);
+            if (iface == null) {
+                throw new RuntimeException("resource " + resourceID + " cap " + cap.getStandardID() + " does not support securityMethod " + securityMethod);
+            }
+            URL turl = iface.getAccessURL().getURL();
+            return turl;
+        } catch (IOException | ResourceNotFoundException ex) {
+            throw new RuntimeException("failed to get capabilities for " +  resourceID, ex);
+        }
+    }
+    
+    private Transfer negotiate(URL turl) throws Exception {
+        
+        Transfer request = new Transfer(target, direction);
+        request.version = VOS.VOSPACE_21;
+        
+        String proto = VOS.PROTOCOL_HTTPS_GET;
+        if (Direction.pushToVoSpace.equals(direction)) {
+            proto = VOS.PROTOCOL_HTTPS_PUT;
+        }
+        
+        if (securityMethod == null) {
+            // all
+            
+            // https+anon
+            Protocol sa = new Protocol(proto);
+            // https+cert
+            Protocol sc = new Protocol(proto);
+            sc.setSecurityMethod(Standards.SECURITY_METHOD_CERT);
+            // https+bearer token
+            Protocol bt = new Protocol(proto);
+            bt.setSecurityMethod(Standards.SECURITY_METHOD_TOKEN);
+
+            request.getProtocols().add(sa);
+            request.getProtocols().add(sc);
+            request.getProtocols().add(bt);
+        } else {
+            // specified
+            Protocol p = new Protocol(proto);
+            p.setSecurityMethod(securityMethod);
+            request.getProtocols().add(p);
+        }
+        
+        if (standardID != null) {
+            Protocol p = new Protocol(standardID.toASCIIString());
+            request.getProtocols().add(p);
+        }
+        
+        TransferWriter writer = new TransferWriter();
+        StringWriter out = new StringWriter();
+        writer.write(request, out);
+        String req = out.toString();
+        log.info("request:\n" + req);
+
+        FileContent content = new FileContent(req, "text/xml", Charset.forName("UTF-8"));
+        HttpPost post = new HttpPost(turl, content, false);
+        post.prepare();
+        InputStream istream;
+
+        if (post.getRedirectURL() != null) {
+            HttpGet get = new HttpGet(post.getRedirectURL(), true);
+            get.prepare();
+            istream = get.getInputStream();
+        } else {
+            istream = post.getInputStream();
+        }
+        String xml = StringUtil.readFromInputStream(istream, "UTF-8");
+        log.info("response:\n" + xml);
+
+        TransferReader reader = new TransferReader();
+        Transfer t = reader.read(xml, null);
+        log.debug("Response transfer: " + t);
+        return t;
+    }
+}
